959985c3bb2537ac4f0ae699d7aa7e5f275baa95
thistle <bcmertz@email.wm.edu>
2018-10-05 17:07:47 -0400

    [added] speed testing script

--- test.sh
+++ test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 echo "Begining Test"
-
-for i in 1 5 25 125
+$(touch results.csv)
+echo "goroutines,time" > results.csv
+for i in 5 10 25 125
 do
-    $(go run torrent.go "https://s3-us-west-1.amazonaws.com/mybucket-bennettmertz/myvideo.mp4" $i)
+    start=$(date +%s.%N)
+    $(go run torrent.go "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4" $i)
+    elapsed=$(echo "$(date +%s.%N) - $start"|bc)
+    echo "$i,$elapsed" >> results.csv
 done
