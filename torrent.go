6090a8b7c78d2d8adcc53f72d1cf3830ba62aab2
thistle <bcmertz@email.wm.edu>
2018-10-05 17:10:25 -0400

    refactored channels and file writing

--- torrent.go
+++ torrent.go
@@ -1,28 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
 	"sync"
-	"time"
 )
 
 var wg sync.WaitGroup
 
 func main() {
-	start := time.Now()
 	var file_path string
 	var chunks int
 	var err error
 
 	custom_arguments := os.Args[1:]
 	if len(custom_arguments) < 2 {
-		// if arguments improperly given then use default arguments
-		file_path = "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
-		chunks = 10
+		file_path = "https://s3-us-west-1.amazonaws.com/mybucket-bennettmertz/myvideo.mp4"
+		chunks = 25
 	} else {
 		file_path = custom_arguments[0]
 		chunks, err = strconv.Atoi(custom_arguments[1])
@@ -31,32 +27,25 @@ func main() {
 		}
 	}
 
-	size := check_size(file_path)
-	chans := make([]chan []byte, chunks)
-	for i := range chans {
-		chans[i] = make(chan []byte, 1)
-		wg.Add(1)
-		start := i * size / chunks
-		end := (i + 1) * size / chunks
-		go download(file_path, start, end, chans[i])
-	}
-
-	wg.Wait()
-
 	file, _ := os.Create("downloaded_video.mp4")
 	defer file.Close()
 
-	for i := range chans {
+	size := check_size(file_path)
+	for i := 0; i < chunks; i++ {
+		wg.Add(1)
 		start := i * size / chunks
-		file.WriteAt(<-chans[i], int64(start))
+		end := (i + 1) * size / chunks
+		go download(file_path, start, end, file)
 	}
-	end := time.Now()
-	elapsed := end.Sub(start)
-	print("time elapsed: %v \n", elapsed)
+
+	wg.Wait()
 }
 
-func download(file_path string, start, end int, channel chan []byte) {
+func download(file_path string, start, end int, file *os.File) {
 	req, err := http.NewRequest("GET", file_path, nil)
+	if err != nil {
+		panic(err)
+	}
 
 	download_range := "bytes=" + strconv.Itoa(start) + "-" + strconv.Itoa(end)
 
@@ -68,14 +57,20 @@ func download(file_path string, start, end int, channel chan []byte) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("download failed")
+		panic(err)
 	}
+
 	defer resp.Body.Close()
 	defer wg.Done()
 
 	body, err := ioutil.ReadAll(resp.Body)
-	channel <- body
-	close(channel)
+	if err != nil {
+		panic(err)
+	}
+	_, err = file.WriteAt(body, int64(start))
+	if err != nil {
+		panic(err)
+	}
 }
 
 func check_size(url string) (length int) {
