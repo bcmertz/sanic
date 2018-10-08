18891cddb2f0fffad10d2440485cbbd08d0ea777
thistle <bcmertz@email.wm.edu>
2018-10-07 20:39:45 -0400

    updated testing goroutines

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Begining Test"
+echo "Beginning Test"
 $(touch results.csv)
 echo "goroutines,time" > results.csv
-for i in 5 10 25 125
+for i in 1 2 5 25 125
 do
     start=$(date +%s.%N)
     $(go run torrent.go -n=$i)
