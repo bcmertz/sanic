0d56f5edab8bdb5e6c0a2b70c8a227f658a8edeb
thistle <bcmertz@email.wm.edu>
2018-10-26 04:23:03 -0400

    tweaked test

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@
 echo "Beginning Test"
 $(touch results.csv)
 echo "goroutines,time" > results.csv
-for i in 1 2 5 25 125
+for i in 2 10 25 50 125 
 do
     start=$(date +%s.%N)
-    $(go run torrent.go -n=$i)
+    $(go run torrent.go --n=$i --v=false)
     elapsed=$(echo "$(date +%s.%N) - $start"|bc) #from online resource
     echo "$i,$elapsed" >> results.csv
 done
