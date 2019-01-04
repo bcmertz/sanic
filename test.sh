e5b00c129c66813a71a046fa59d43cee4ed3fd63
thistle <bcmertz@email.wm.edu>
2019-01-04 13:23:52 -0500

    testing on localhost

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ echo "goroutines,time" > results.csv
 for i in 2 10 25 50 125 
 do
     start=$(date +%s.%N)
-    $(go run torrent.go --n=$i --v=false)
+    $(go run torrent.go --n=$i --v=false --u=http://localhost:8080/mp4)
     elapsed=$(echo "$(date +%s.%N) - $start"|bc) #from online resource
     echo "$i,$elapsed" >> results.csv
 done
